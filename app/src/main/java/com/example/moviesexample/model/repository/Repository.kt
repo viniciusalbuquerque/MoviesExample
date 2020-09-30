08a96dac5fbc79453715e692aea924f599227172
Vinicius Albuquerque <vinicius.aalbuquerque@gmail.com>
2020-09-30 03:28:30 -0300

    Issue #2: List all the movies
    Building the first screen of the app where the user will be able to
    see a variaty of movies. Since the api we're using for now doesn't
    have a "get random movies", we're showing movies that are result from
    a search for the word "car" (but it could be any word).
    
    WIP
    
    Change-Id: I0d65259419e0a5233ca86aadf1b512113e636666

--- app/src/main/java/com/example/moviesexample/model/repository/Repository.kt
+++ app/src/main/java/com/example/moviesexample/model/repository/Repository.kt
@@ -1,10 +1,8 @@
 package com.example.moviesexample.model.repository
 
-import kotlinx.coroutines.flow.Flow
-
 interface Repository<T> {
-    suspend fun fetch(): Flow<List<T>>
-    suspend fun getFavorites(): Flow<List<T>>
-    suspend fun getById(id: String): Flow<T>
-    suspend fun searchMovie(searchString: String): Flow<List<T>>
+    suspend fun fetch(page: Int): List<T>
+    suspend fun getFavorites(): List<T>
+    suspend fun getById(id: String): T
+    suspend fun search(searchString: String): List<T>
 }
---
+++ app/src/test/java/com/example/moviesexample/viewmodel/MovieListViewModelTest.kt
@@ -0,0 +1,61 @@
+package com.example.moviesexample.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.example.moviesexample.model.data.Movie
+import com.example.moviesexample.model.repository.MovieRepositoryMocked
+import com.example.moviesexample.model.repository.MoviesRepository
+import com.example.moviesexample.model.repository.Repository
+import com.example.moviesexample.util.AndroidLogger
+import com.example.moviesexample.util.Logger
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+
+class MovieListViewModelTest {
+
+    @Rule @JvmField
+    val rule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var logger: Logger
+
+    @Mock
+    private lateinit var mockObserver: Observer<List<Movie>>
+
+    private lateinit var movieListViewModel: MovieListViewModel
+    private lateinit var movieRepository: MovieRepositoryMocked
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        movieRepository = MovieRepositoryMocked()
+        movieListViewModel = MovieListViewModel(movieRepository, logger)
+        movieListViewModel.moviesLiveData.observeForever(mockObserver)
+    }
+
+    @Test
+    fun testFetchFunction() {
+        movieListViewModel.fetchNextPage()
+        verify(mockObserver).onChanged(MovieRepositoryMocked.movieList)
+    }
+
+    @Test
+    fun testSearchFunction() {
+        movieListViewModel.searchMovie("")
+        verify(mockObserver).onChanged(MovieRepositoryMocked.movieList)
+    }
+
+    @Test
+    fun testGetMovieById() {
+        movieListViewModel.getMovieById("")
+        verify(mockObserver).onChanged(MovieRepositoryMocked.movieList.subList(0,1))
+    }
+
+}
