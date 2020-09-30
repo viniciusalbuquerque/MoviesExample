922b252352768f7b74466292b976f353b7fa159e
Vinicius Albuquerque <vinicius.aalbuquerque@gmail.com>
2020-09-30 03:28:30 -0300

    Issue #2: List all the movies
    Building the first screen of the app where the user will be able to
    see a variaty of movies. Since the api we're using for now doesn't
    have a "get random movies", we're showing movies that are result from
    a search for the word "car" (but it could be any word).
    
    Change-Id: I0d65259419e0a5233ca86aadf1b512113e636666

---
+++ app/src/main/java/com/example/moviesexample/view/listeners/EndlessScrollCallback.kt
@@ -0,0 +1,7 @@
+package com.example.moviesexample.view.listeners
+
+fun interface EndlessScrollCallback {
+
+    fun loadNext()
+
+}
--- app/src/main/java/com/example/moviesexample/view/ui/ListMoviesFragment.kt
+++ app/src/main/java/com/example/moviesexample/view/ui/ListMoviesFragment.kt
@@ -11,15 +11,20 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.moviesexample.databinding.MovieListFragmentBinding
 import com.example.moviesexample.model.data.Movie
 import com.example.moviesexample.util.AndroidLogger
+import com.example.moviesexample.util.Logger
+import com.example.moviesexample.view.adapter.LinearLayoutManagerImpl
 import com.example.moviesexample.view.adapter.MovieListAdapter
+import com.example.moviesexample.view.listeners.EndlessScrollListener
 import com.example.moviesexample.viewmodel.MovieListViewModel
 import com.example.moviesexample.viewmodel.ViewModelProvider
 
 class ListMoviesFragment: Fragment() {
 
+    private val TAG = "ListMoviesFragment"
     private lateinit var binding: MovieListFragmentBinding
     private lateinit var viewModel: MovieListViewModel
-    private lateinit var adapter:MovieListAdapter
+    private lateinit var adapter: MovieListAdapter
+    private lateinit var logger: Logger
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,26 +38,38 @@ class ListMoviesFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider.createMovieListViewModel(view.context.applicationContext, this)
+        logger = AndroidLogger()
 
         buildRecyclerView()
         viewModel.moviesLiveData.observe(viewLifecycleOwner, Observer {
-            AndroidLogger.degub(it.toString())
+            logger.debug(TAG, it.toString())
             refreshList(it)
         })
 
         lifecycleScope.launchWhenStarted {
-            viewModel.fetchMovies()
+            loadNext()
         }
     }
 
     private fun buildRecyclerView() {
+        val layoutManager = LinearLayoutManager(context)
+        val linearLayoutManager = LinearLayoutManagerImpl(layoutManager)
+
         adapter = MovieListAdapter()
-        binding.movieListRecyclerView.layoutManager = LinearLayoutManager(context)
+        binding.movieListRecyclerView.layoutManager = layoutManager
         binding.movieListRecyclerView.adapter = adapter
+        binding.movieListRecyclerView.addOnScrollListener(
+            EndlessScrollListener(linearLayoutManager, { loadNext() }, AndroidLogger())
+        )
     }
 
     private fun refreshList(movies: List<Movie>) {
-        adapter.setMovies(movies)
+        adapter.submitMovies(movies)
+    }
+
+    private fun loadNext() {
+        logger.debug(TAG, "loadNext called")
+        viewModel.fetchNextPage()
     }
 
 }
