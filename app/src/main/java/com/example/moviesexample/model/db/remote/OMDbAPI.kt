035609887030e74f02c47e7921c0d17ab876d366
Vinicius Albuquerque <vinicius.aalbuquerque@gmail.com>
2020-09-30 03:28:30 -0300

    Issue #2: List all the movies
    Building the first screen of the app where the user will be able to
    see a variaty of movies. Since the api we're using for now doesn't
    have a "get random movies", we're showing movies that are result from
    a search for the word "car" (but it could be any word).
    
    WIP
    
    Change-Id: I0d65259419e0a5233ca86aadf1b512113e636666

--- app/src/main/java/com/example/moviesexample/model/db/remote/OMDbAPI.kt
+++ app/src/main/java/com/example/moviesexample/model/db/remote/OMDbAPI.kt
@@ -1,11 +1,13 @@
 package com.example.moviesexample.model.db.remote
 
 import com.example.moviesexample.model.data.MovieRemote
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.lang.Exception
 
-class OMDbAPI(private val apiKey: String):
+class OMDbAPI(private val apiKey: String) :
     DBService<MovieRemote> {
 
     private val url = "http://www.omdbapi.com/"
@@ -20,13 +22,18 @@ class OMDbAPI(private val apiKey: String):
         retrofitService = retrofit.create(OMDbRetrofitService::class.java)
     }
 
-    override suspend fun get(page: Int): List<MovieRemote> =
-         retrofitService.getMovies(apiKey, page.toString()).body()?.movieRemoteList ?: listOf()
+    override suspend fun get(page: Int): List<MovieRemote> = withContext(Dispatchers.IO) {
+        retrofitService.getMovies(apiKey, page.toString()).body()?.movieRemoteList ?: listOf()
+    }
 
-    override suspend fun getById(id: String) = retrofitService.getMovieById(apiKey, id).body()
+    override suspend fun getById(id: String) = withContext(Dispatchers.IO) {
+        retrofitService.getMovieById(apiKey, id).body()
             ?: throw Exception("Movie with id = $id not found")
+    }
 
-    override suspend fun search(searchString: String, page: Int) =
-        retrofitService.searchMovies(apiKey, page.toString(), searchString).body()?.movieRemoteList
+    override suspend fun search(searchString: String, page: Int) = withContext(Dispatchers.IO) {
+        retrofitService.searchMovies(apiKey, page.toString(), searchString)
+            .body()?.movieRemoteList
             ?: listOf()
+    }
 }
