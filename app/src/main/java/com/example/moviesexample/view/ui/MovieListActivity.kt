a16a7c1d724fadd4e5bf59bf33841428b8939c5d
Vinicius Albuquerque <vinicius.aalbuquerque@gmail.com>
2021-02-27 21:56:07 -0300

    Add Bottom Navigation View using Navigation component
    Adding a bottom navigation view so we can go through the different
    fragments we have for now.
    
    Change-Id: Ic1a9f6b88b8c7ae93f96273b58c6efb5bbe75fd9

--- app/src/main/java/com/example/moviesexample/view/ui/MovieListActivity.kt
+++ app/src/main/java/com/example/moviesexample/view/ui/MovieListActivity.kt
@@ -2,6 +2,11 @@ package com.example.moviesexample.view.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.moviesexample.R
 import com.example.moviesexample.databinding.MainAcitivityBinding
 
 class MovieListActivity: AppCompatActivity() {
@@ -13,11 +18,11 @@ class MovieListActivity: AppCompatActivity() {
         binding = MainAcitivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val movieListFragment = ListMoviesFragment()
-//        val favMoviesFragment = FavoriteMoviesFragment()
-// testing
-        supportFragmentManager.beginTransaction().apply {
-            add(binding.fragmentContainerView.id, movieListFragment)
-        }.commit()
+        val bottomNavigationView = binding.bottomNavigationView
+        val navigationContoller = findNavController(R.id.main_nav_host)
+        val appBarConfig = AppBarConfiguration(setOf(R.id.listMoviesFragment, R.id.favoriteMoviesFragment))
+
+        setupActionBarWithNavController(navigationContoller, appBarConfig)
+        bottomNavigationView.setupWithNavController(navigationContoller)
     }
 }
